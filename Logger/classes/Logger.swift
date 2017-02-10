61caebbcb0e2df8f9b78fbe157f5ecef18cbf8f0
Kohei Tabata <nerd0geek1@gmail.com>
2017-02-10 09:53:32 +0900

    add SwiftLint Aggregate to manage SwiftLint easily

--- Logger/classes/Logger.swift
+++ Logger/classes/Logger.swift
@@ -13,7 +13,7 @@ final public class Logger {
     static public var shouldOutput: Bool   = true
     static public var useXcodeColors: Bool = true
 
-    //MARK: - public
+    // MARK: - public
 
     final public class func LOGError<T>(_ value: T, fileName: String = #file, line: Int = #line, methodName: String = #function) {
         LOG(value, fileName: fileName, line: line, methodName: methodName, red: 255, green: 0, blue: 0)
@@ -31,22 +31,22 @@ final public class Logger {
         LOG(value, fileName: fileName, line: line, methodName: methodName, red: 0, green: 0, blue: 255)
     }
 
-    //MARK: - private
+    // MARK: - private
 
     private class func LOG<T>(_ value: T, fileName: String, line: Int, methodName: String, red: Float = 0, green: Float = 0, blue: Float = 0) {
         if !shouldOutput {
             return
         }
 
-        let ESCAPE: String   = "\u{001b}["
-        let RESET: String    = ESCAPE + ";"
         let fileName: String = fileName.lastPathComponent.deletingPathExtension
         let string: String   = "\n\nclass:\(fileName)\nline:\(line)\nmethod:\(methodName)\n\(value)\n\n"
 
-        let coloringParams: String = "fg\(Int(red)),\(Int(green)),\(Int(blue));"
-
         let text: String
         if useXcodeColors {
+            let ESCAPE: String         = "\u{001b}["
+            let coloringParams: String = "fg\(Int(red)),\(Int(green)),\(Int(blue));"
+            let RESET: String          = ESCAPE + ";"
+
             text = "\(ESCAPE)\(coloringParams)\(string)\(RESET)"
         } else {
             text = string
